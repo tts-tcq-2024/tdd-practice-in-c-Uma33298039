ee4d6ab3d396d76e949ea514984b92aac4f7185c
Uma33298039 <umamageshwari098@gmail.com>
2024-07-11 16:46:03 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,26 +1,15 @@
-#include <stdio.h>
-#include "StringCalculator.h"
-#include <stdlib.h>
-#include <string.h>
-
-int add(const char* input) {
-    if (input == NULL || strcmp(input, "") == 0) {
-        return 0;
+bool isEmptyStr(const char*input)
+{
+  if (input == NULL || input[0] == '\0') {
+        return true; // The string is empty
     }
-
-    int sum = 0;
-    char *buffer = strdup(input); // Make a copy since strtok modifies the string
-    char *token = strtok(buffer, ",\n"); // Tokenize by comma or newline
-
-    while (token != NULL) {
-        int num = atoi(token);
-        if (num <= 1000) { // Ignore numbers greater than 1000
-            sum += num;
-        }
-        token = strtok(NULL, ",\n");
-    }
-
-    free(buffer); // Free allocated memory
-    return sum;
+  return false;
 }
-
+int add (const char*input)
+{
+  if(isEmptyStr(input) ==true)
+{
+  return 0;
+}
+  return 1;
+}
