1e1083714a20c1b95cb7440dd9481358174b8008
Uma33298039 <umamageshwari098@gmail.com>
2024-07-14 08:22:16 +0530

    Update StringCalculator_Final.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,5 +1,6 @@
 #include <string.h>
 #include<stdlib.h>
+// ExpectZeroForEmptyInput, ExpectZeroForSingleZero  
 bool isemptystring(const char* value)
 {
     if (value == NULL || value[0] == '\0')
@@ -8,30 +9,44 @@ bool isemptystring(const char* value)
     }
     return 0;
 }
+
 int islessthanthousand(const char *val)
 {    
     int sum = 0;
-    int value_check = atoi(val);
-    if(value_check < 1000)
+    int input = atoi(val); // string to int
+    if(input < 1000)
     {
-        sum += value_check;
+        sum += input;
         return sum;
     }
     return 0;
      
 }
 
+int calculatesum(const char* input, char* delimiter)
+{
+    int sum = 0;
+    char* duplicate_input = NULL;
+    duplicate_input = strdup (input); // duplicate the string 
+    char* token = strtok(duplicate_input,delimiter); //tokenizing strings
+    while(token != NULL)
+    {
+        sum = sum + islessthanthousand(token);
+        token = strtok(NULL,delimiter);
+    }
+    return sum;
+}
 
 void append_custom_delimiter(const char* input, char* delimiter) {
     int i = 2; 
     while(input[i] != '\n')
     {
-        char temp[1] = {input[i]};
-        strncat (delimiter, temp,1);
+        char src[1] = {input[i]};
+        strncat (delimiter, src,1);
         i = i+1;
     }
 }
-
+//ExpectSumWithCustomDelimiter("//;\n1;2")
 void checkcustomdelimiter(const char* input, char* delimiter)
 {
     if (input[0] == '/' && input[1] == '/')
@@ -44,21 +59,7 @@ void checkcustomdelimiter(const char* input, char* delimiter)
         delimiter = ",\n";
     }
 }
-
-int calculatesum(const char* input, char* delimiter)
-{
-    int sum = 0;
-    char* dup_input = NULL;
-    dup_input = strdup (input);
-    char* input_seg = strtok(dup_input,delimiter);
-    while(input_seg != NULL)
-    {
-        sum = sum + islessthanthousand(input_seg);
-        input_seg = strtok(NULL,delimiter);
-    }
-    return sum;
-}
-
+//ExpectSumWithCustomDelimiter
 int add (const char* input)
 {
     char delimiter[128] = ",\n";
