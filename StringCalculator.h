f99f377d88277de2f8e126a15bddaa94f7c1e76c
Uma33298039 <umamageshwari098@gmail.com>
2024-07-14 07:56:00 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,15 +1,72 @@
-bool isEmptyStr(const char*input)
+#include <string.h>
+#include<stdlib.h>
+Bool isemptystring(const char* value)
 {
-  if (input == NULL || input[0] == '\0') {
-        return true; // The string is empty
+    if (value == NULL || value[0] == '\0')
+    {
+        return 1;
     }
-  return false;
+    return 0;
 }
-int add (const char*input)
-{
-  if(isEmptyStr(input) ==true)
-{
-  return 0;
+int islessthanthousand(const char *val)
+{    
+    int sum = 0;
+    int value_check = atoi(val);
+    if(value_check < 1000)
+    {
+        sum += value_check;
+        return sum;
+    }
+    return 0;
+     
 }
-  return 1;
+
+
+void append_custom_delimiter(const char* input, char* delimiter) {
+    int i = 2; 
+    while(input[i] != '\n')
+    {
+        char temp[1] = {input[i]};
+        strncat (delimiter, temp,1);
+        i = i+1;
+    }
+}
+
+void checkcustomdelimiter(const char* input, char* delimiter)
+{
+    if (input[0] == '/' && input[1] == '/')
+    {
+        strcpy(delimiter, "");
+        append_custom_delimiter(input,delimiter);
+    }
+    else
+    {
+        delimiter = ",\n";
+    }
+}
+
+int calculatesum(const char* input, char* delimiter)
+{
+    int sum = 0;
+    char* dup_input = NULL;
+    dup_input = strdup (input);
+    char* input_seg = strtok(dup_input,delimiter);
+    while(input_seg != NULL)
+    {
+        sum = sum + islessthanthousand(input_seg);
+        input_seg = strtok(NULL,delimiter);
+    }
+    return sum;
+}
+
+int add (const char* input)
+{
+    char delimiter[128] = ",\n";
+    if (1 == isemptystring(input))
+    {
+        return 0;
+    }
+    checkcustomdelimiter(input, delimiter);
+    return calculatesum(input, delimiter);
+    
 }
